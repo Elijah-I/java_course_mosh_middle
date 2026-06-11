f1c2bd5200a90b6cf3acc588b3a1767e97e53183
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2026-06-11 16:10:29 +0300

    refactor: Migrate Mortgage

--- src/me/middle/Main.java
+++ src/me/middle/Main.java
@@ -1,39 +1,16 @@
 package me.middle;
 
-import java.text.NumberFormat;
-
 public class Main {
-    static void main(String[] args) {
-        int principal = DataReader.getInt("Principal (1.000 - 1.000.000): ", 1_000, 1_000_000);
-        byte years = DataReader.getByte("Period (Years): ", 1, 30);
-        float rate = DataReader.getFloat("Annual interest rate: ", 1, 30);
+    static void main() {
+        var mortgage = new Mortgage(
+                1_000,
+                1_000_000,
+                1,
+                30,
+                1,
+                30);
 
-        double mortgage = calculateMortgage(principal, years, rate);
-
-        printSchedule(mortgage, principal);
-    }
-
-    public static void printSchedule(double mortgage, double principal) {
-        NumberFormat number = NumberFormat.getCurrencyInstance();
-
-        System.out.println("MORTGAGE\n----------\nMonthly payments: " + number.format(mortgage) + "\n\nPAYMENT SCHEDULE\n----------");
-
-        while (principal > 0) {
-            principal -= mortgage;
-            System.out.println(number.format(Math.max(principal, 0)));
-        }
-    }
-
-    private static double calculateMortgage(int principal, byte years, float rate) {
-        final byte MONTH_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
-        int period = years * MONTH_IN_YEAR;
-        float monthRate = rate / MONTH_IN_YEAR / PERCENT;
-
-        double ratedPeriod = Math.pow(1 + monthRate, period);
-        double total = (principal * (monthRate * ratedPeriod) / (ratedPeriod - 1));
-
-        return total;
+        mortgage.printMortgage();
+        mortgage.printSchedule();
     }
 }
---
+++ src/me/middle/Mortgage.java
@@ -0,0 +1,52 @@
+package me.middle;
+
+import java.text.NumberFormat;
+
+public class Mortgage {
+    private final double mortgage;
+    private final int principal;
+    private final byte years;
+    private final float rate;
+
+    final byte PERCENT = 100;
+    final byte MONTH_IN_YEAR = 12;
+    NumberFormat number = NumberFormat.getCurrencyInstance();
+
+    public Mortgage(
+            int principalMin,
+            int principalMax,
+            int periodMin,
+            int periodMax,
+            int annualMin,
+            int annualMax
+    ) {
+        principal = DataReader.getInt("Principal (1.000 - 1.000.000): ", principalMin, principalMax);
+        rate = DataReader.getFloat("Annual interest rate: ", annualMin, annualMax);
+        years = DataReader.getByte("Period (Years): ", periodMin, periodMax);
+
+        mortgage = this.calculateMortgage();
+    }
+
+    private double calculateMortgage() {
+        int period = years * MONTH_IN_YEAR;
+        float monthRate = rate / MONTH_IN_YEAR / PERCENT;
+        double ratedPeriod = Math.pow(1 + monthRate, period);
+
+        return (principal * (monthRate * ratedPeriod) / (ratedPeriod - 1));
+    }
+
+    public void printMortgage() {
+        System.out.println("MORTGAGE\n----------\nMonthly payments: " + number.format(mortgage));
+    }
+
+    public void printSchedule() {
+        System.out.println("\n\nPAYMENT SCHEDULE\n----------");
+        
+        int totalPrincipal = principal;
+
+        while (totalPrincipal > 0) {
+            totalPrincipal -= (int) mortgage;
+            System.out.println(number.format(Math.max(totalPrincipal, 0)));
+        }
+    }
+}
